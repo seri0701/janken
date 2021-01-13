cb77f1b0c49607edc7ed77e1e2d0277ad113c643
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-01-14 06:49:42 +0900

    v1

--- app.js
+++ app.js
@@ -1,46 +1,97 @@
-// 
-const GU    = 1;
-const CHOKI = 2;
-const PA    = 3;
+function janken() {
+  /* 定数定義 ************************/
+  // ジャンケンの手の番号を設定
+  const GU = 1;
+  const CHOKI = 2;
+  const PA = 3;
 
-// ジャンケンの入力ダイアログボックスを表示
-let hum =prompt('半角数字で１〜３の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
-hum = parseInt(hum, 10);
-
-// 入力値のチェック
-if (hum !== GU && hum!== CHOKI && hum!== PA) {
-  // 入力値が不適切な場合
-  alert('入力値をうまく認識できませんでした。ブラウザを再読み込みすると、もう一度挑戦できます。');
-} else {
+  /* 関数定義 ************************/
+  // 人間に手を入力してもらう機能
+  function getHumHand() {
+    let hum = prompt(
+      "半角数字で1~3の数字を入力してください。\n\n" +
+        GU +
+        ":グー\n" +
+        CHOKI +
+        ":チョキ\n" +
+        PA +
+        ":パー"
+    );
+    hum = parseInt(hum, 10);
+    if (hum !== GU && hum !== CHOKI && hum !== PA) {
+      return undefined;
+    } else {
+      return hum;
+    }
+  }
 
   // コンピュータの手を決める
-  let com = Math.floor(Math.random() * 3) + 1;
+  function getComHand() {
+    return Math.floor(Math.random() * 3) + 1;
+  }
 
-  //コンピュータの手の名前
-  let comHandName = '';
-  switch (com) {
-        case GU:
-            comHandName = 'グー';
-            break;
-        case CHOKI:
-            comHandName = 'チョキ';
-            break;
-        case PA:
-            comHandName = 'パー';
-            break;
+  // コンピュータの手の名前を取得
+  function getHandName(num) {
+    switch (num) {
+      case GU:
+        return "グー";
+      case CHOKI:
+        return "チョキ";
+      case PA:
+        return "パー";
+    }
   }
 
   // 結果の判定
-  let msgResult = '';
-  if (hum === com) {
-      msgResult = '結果はあいこでした。';
-  } else if ((com === GU && hum === PA) || (com === CHOKI && hum === GU) || (com === PA && hum === CHOKI)) {
-      msgResult = '勝ちました。'
-  } else {
-      msgResult = '負けました。'
+  function getResult(com, hum) {
+    if (hum === com) {
+      return "結果はあいこでした。";
+    } else if (
+      (com === GU && hum === PA) ||
+      (com === CHOKI && hum === GU) ||
+      (com === PA && hum === CHOKI)
+    ) {
+      return "勝ちました。";
+    } else {
+      return "負けました。";
+    }
   }
 
-  //最終的な結果の表示
-  msgResult = msgResult + 'コンピュータの出した手は「' + comHandName + '」でした';
-  alert(msgResult);
- } 
+  // 最終的な結果のメッセージ
+  function getResultMsg(com, hum) {
+    return (
+      getResult(com, hum) +
+      "コンピュータの出した手は「" +
+      getHandName(com) +
+      "」でした"
+    );
+  }
+
+  /* 実行する処理 ************************/
+  let hum = getHumHand();
+  if (!hum) {
+    alert(
+      "入力値をうまく認識できませんでした。ブラウザを再読込すると、もう一度挑戦できます。"
+    );
+  } else {
+    let com = getComHand();
+    alert(getResultMsg(com, hum));
+    return getResult(com, hum);
+  }
+}
+
+let win = 0;
+let isLose = false;
+while (!isLose) {
+  let result = janken();
+  if (result === "結果はあいこでした。") {
+    continue;
+  }
+  if (result === "勝ちました。") {
+    win++;
+    alert("ただいま「" + win + "」勝です。");
+    continue;
+  }
+  alert("連勝はストップです。記録は「" + win + "」勝でした。");
+  isLose = true;
+}
